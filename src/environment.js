e8e55f9257cc6796109f9bd5765eeb9fb500ddea
XandeCoding <af.santos1998@gmail.com>
2020-10-18 11:37:03 -0300

    fixed error in variables

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,5 @@
 const environment = {
-  DATABASE_USERNAME: process.env.DATABASE_PASSWORD || 'postgres',
+  DATABASE_USERNAME: process.env.DATABASE_USERNAME || 'postgres',
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'moviemoji',
   DATABASE_HOST: process.env.DATABASE_HOST || '127.0.0.1',
   DATABASE_PORT: process.env.DATABASE_PORT || '5432',
