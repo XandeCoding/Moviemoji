06a28a475e1c58900801ece2542ded66293a71cc
XandeCoding <af.santos1998@gmail.com>
2020-10-18 11:16:16 -0300

    changed environmment variable name

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,7 @@
 const environment = {
   DATABASE_USERNAME: process.env.DATABASE_PASSWORD || 'postgres',
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'moviemoji',
-  DATABASE_URL: process.env.DATABASE_URL || '127.0.0.1',
+  DATABASE_HOST: process.env.DATABASE_HOST || '127.0.0.1',
   DATABASE_PORT: process.env.DATABASE_PORT || '5432',
   DATABASE_NAME: process.env.DATABASE_NAME || 'postgres'
 }
